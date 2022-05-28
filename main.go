c04662a45da99b31929a30dde5982495fda11090
Nikita Pirozhkov <14nikita14m@gmail.com>
2022-05-28 23:56:02 +0400

    Delete comments

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"HomeTasksForGoCourse/solutions/findmissingelem"
 	"HomeTasksForGoCourse/solutions/oddoccurrences"
 	"fmt"
 )
@@ -8,20 +9,20 @@ import (
 func main() {
 	// Тесты Поиска отсутствующего элемента
 
-	//slice1 := []int{2, 3, 1, 5}
-	//fmt.Println(findmissingelem.Solution(slice1))
-	//
-	//slice2 := []int{7, 2, 3, 1, 5, 4}
-	//fmt.Println(findmissingelem.Solution(slice2))
-	//
-	//slice3 := []int{2, 3, 4, 5, 6}
-	//fmt.Println(findmissingelem.Solution(slice3))
-	//
-	//slice4 := []int{1, 2, 3, 4, 6}
-	//fmt.Println(findmissingelem.Solution(slice4))
-	//
-	//slice5 := []int{}
-	//fmt.Println(findmissingelem.Solution(slice5))
+	slice1 := []int{2, 3, 1, 5}
+	fmt.Println(findmissingelem.Solution(slice1))
+
+	slice2 := []int{7, 2, 3, 1, 5, 4}
+	fmt.Println(findmissingelem.Solution(slice2))
+
+	slice3 := []int{2, 3, 4, 5, 6}
+	fmt.Println(findmissingelem.Solution(slice3))
+
+	slice4 := []int{1, 2, 3, 4, 6}
+	fmt.Println(findmissingelem.Solution(slice4))
+
+	slice5 := []int{}
+	fmt.Println(findmissingelem.Solution(slice5))
 
 	// Тесты Чудных вхождений в массив
 
