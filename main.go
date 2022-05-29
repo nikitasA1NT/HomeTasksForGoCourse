e928d3dab768903d44f8939155aea55db9693c6c
Nikita Pirozhkov <14nikita14m@gmail.com>
2022-05-29 15:16:52 +0400

    Test of find missing elem func from main to spec func

--- main.go
+++ main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
+	"HomeTasksForGoCourse/solutions/findmissingelem"
 	"HomeTasksForGoCourse/solutions/oddoccurrences"
 	"fmt"
 )
 
 func main() {
 	// Тесты Поиска отсутствующего элемента
-
-	//slice1 := []int{2, 3, 1, 5}
-	//fmt.Println(findmissingelem.Solution(slice1))
-	//
-	//slice2 := []int{7, 2, 3, 1, 5, 4}
-	//fmt.Println(findmissingelem.Solution(slice2))
-	//
-	//slice3 := []int{2, 3, 4, 5, 6}
-	//fmt.Println(findmissingelem.Solution(slice3))
-	//
-	//slice4 := []int{1, 2, 3, 4, 6}
-	//fmt.Println(findmissingelem.Solution(slice4))
-	//
-	//slice5 := []int{}
-	//fmt.Println(findmissingelem.Solution(slice5))
+	PrintTestsOfFindMissingElem()
 
 	// Тесты Чудных вхождений в массив
 	PrintTestsOfOddOccurrences()
+}
+
+func PrintTestsOfFindMissingElem() {
+	slice1 := []int{2, 3, 1, 5}
+	fmt.Println(findmissingelem.Solution(slice1))
+
+	slice2 := []int{7, 2, 3, 1, 5, 4}
+	fmt.Println(findmissingelem.Solution(slice2))
+
+	slice3 := []int{2, 3, 4, 5, 6}
+	fmt.Println(findmissingelem.Solution(slice3))
+
+	slice4 := []int{1, 2, 3, 4, 6}
+	fmt.Println(findmissingelem.Solution(slice4))
+
+	slice5 := []int{}
+	fmt.Println(findmissingelem.Solution(slice5))
 }
 
 func PrintTestsOfOddOccurrences() {
